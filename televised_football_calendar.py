238224a35c90b6a4ec5f27858f3f2098bed141a9
Jamie Stewart <jamie.stewart5@gmail.com>
2019-11-04 23:15:51 +0000

    Convert to python3 & separate competitions per ics

--- televised_football_calendar.py
+++ televised_football_calendar.py
@@ -1,20 +1,20 @@
-import requests
+#  Generate a football calendar
 from lxml import html
 from user_agent import generate_user_agent
 from ics.icalendar import Calendar, Event
 from ics.alarm import DisplayAlarm
 from datetime import timedelta
+import requests
 import arrow
 import argparse
 import datetime
 import sys
 import os
 import csv
-import unicodedata
 
 
 program = argparse.ArgumentParser(description='Generate an .ics file of the televised Premier League fixtures')
-program.add_argument('output_file', help='The absolute path to the output .ics file you wish to place the fixtures in')
+program.add_argument('output_dir', help='The absolute path to the directory to place the .ics files in')
 program.add_argument('--alert_minutes', type=int, help='The number of minutes before kick-off that you want an alert')
 
 headers = {
@@ -23,16 +23,22 @@ headers = {
 
 SRC_DIR = os.path.dirname(sys.argv[0])
 
-def formatted_datetime():
+
+def _formatted_datetime():
     return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+
+def _output_path(competition_name, output_dir):
+    formatted_competition_name = competition_name.lower().strip().replace('\'', '').replace(' ', '-')
+    return os.path.join(output_dir, f'{formatted_competition_name}.ics')
 
 
 def get_fixtures_for_competition(fixtures_url):
 
-    print 'Getting games for competition: {}'.format(fixtures_url)
+    print(f'Getting games for competition: {fixtures_url}')
 
     fixtures_page = requests.get(fixtures_url, headers=headers)
-    html_tree = html.fromstring(fixtures_page.content)     
+    html_tree = html.fromstring(fixtures_page.content)
 
     listings_rows = html_tree.xpath('//div[@id="listings"]/div[@class="container"]/div[@class="row-fluid"]')
 
@@ -42,18 +48,18 @@ def get_fixtures_for_competition(fixtures_url):
     for row in listings_rows:
 
         date_elem = row.xpath('./div[contains(@class, "matchdate")]')
-        if date_elem: #Have a new match date
+        if date_elem:  # Have a new match date
             if 'No Upcoming TV Fixtures' in date_elem[0].text:
-                print 'No games for competition {}'.format(fixtures_url)
+                print(f'No games for competition {fixtures_url}')
                 return games
 
-            match_date = arrow.get(date_elem[0].text, 'dddd Do MMMM YYYY', tzinfo='Europe/London')    
+            match_date = arrow.get(date_elem[0].text, 'dddd Do MMMM YYYY', tzinfo='Europe/London')
 
-        else: #This is a game
+        else:  # This is a game
             fixture = ''
             try:
                 home_team, away_team = [x.strip() for x in row.xpath('./div[contains(@class, "matchfixture")]')[0].text.split(' v ')]
-                fixture = '{} vs {}'.format(home_team, away_team)
+                fixture = f'{home_team} vs {away_team}'
             except ValueError:
                 if row.xpath('./div[contains(@class, "matchfixture")]')[0].text.strip() == 'TBC':
                     fixture = 'TBC'
@@ -65,12 +71,12 @@ def get_fixtures_for_competition(fixtures_url):
             if 'TBC' in kickoff_time_text:
                 continue
             hour, minute = kickoff_time_text.split(':')
-            
+
             kick_off = match_date.replace(hour=int(hour), minute=int(minute))
 
-            #Seem to have &nbsp; characters in their text
-            competition = row.xpath('./div[contains(@class, "competition")]')[0].text.strip().encode('ascii', 'ignore')
-            
+            # Seem to have &nbsp; characters in their text
+            competition = row.xpath('./div[contains(@class, "competition")]')[0].text.strip()
+
             channel_text = row.xpath('./div[contains(@class, "channels")]')[0].text.lower()
             if 'sky' in channel_text:
                 channel = 'Sky Sports'
@@ -95,49 +101,51 @@ def get_fixtures_for_competition(fixtures_url):
             elif 'celtic' in channel_text:
                 channel = 'Celtic TV'
             else:
-                raise ValueError('Television channel not recognised: {}'.format(channel_text))
-            
+                raise ValueError(f'Television channel not recognised: {channel_text}')
+
             game = {
                 'fixture': fixture,
                 'kick_off': kick_off,
                 'channel': channel,
                 'competition': competition
             }
-            games.append(game)  
+            games.append(game)
 
     return games
+
 
 def generate_calendar_for_games(games_array):
     calendar = Calendar()
 
     for game in games:
         event = Event()
-        event.name = '{} ({})'.format(game['fixture'], game['competition'])
+        event.name = f'{game["fixture"]} ({game["competition"]})'
         start = game['kick_off']
         event.begin = start
         event.end = start.shift(hours=+2)
         event.location = game['channel']
         if args.alert_minutes:
             alarm = DisplayAlarm(trigger=timedelta(minutes=args.alert_minutes))
-            event.alarms.add(alarm)
+            event.alarms.append(alarm)
         calendar.events.add(event)
-    
+
     return calendar
 
+
 if __name__ == '__main__':
-    
+
     args = program.parse_args()
-    games = []
 
     competitions_file = os.path.join(SRC_DIR, 'competitions.csv')
-    with open (competitions_file, 'r') as csvfile:
+    with open(competitions_file, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
 
         for row in reader:
-            current_length = len(games)
-            games.extend(get_fixtures_for_competition(row['URL']))
-            print '{} Got {} games for {}'.format(formatted_datetime(), len(games) - current_length, row['NAME'])
+            games = get_fixtures_for_competition(row['URL'])
+            print(f'{_formatted_datetime()} Got {len(games)} games for {row["NAME"]}')
 
-    with open(args.output_file, 'w') as ics_file:
-        print '{} Writing games to {}'.format(formatted_datetime(), os.path.abspath(args.output_file))
-        ics_file.writelines(generate_calendar_for_games(games))
+            output_path = _output_path(row['NAME'], args.output_dir)
+            with open(output_path, 'w') as ics_file:
+
+                print(f'{_formatted_datetime()} Writing games to {output_path}')
+                ics_file.writelines(generate_calendar_for_games(games))
